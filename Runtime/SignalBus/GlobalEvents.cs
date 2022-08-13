6359d51dba484a3bbd396a6515465117c2e91a5c
sorenbn <sorenbn@outlook.com>
2022-08-13 17:32:30 +0200

    feat: made it dumb and simple

--- Runtime/SignalBus/GlobalEvents.cs
+++ Runtime/SignalBus/GlobalEvents.cs
@@ -5,38 +5,22 @@ namespace SBN.Events
 {
     public static class GlobalEvents<TEvent> where TEvent : new()
     {
-        private static Dictionary<Type, List<Action<TEvent>>> eventListeners = new Dictionary<Type, List<Action<TEvent>>>();
+        private static HashSet<Action<TEvent>> subscriptions = new HashSet<Action<TEvent>>();
 
         public static void Publish(TEvent args)
         {
-            var type = typeof(TEvent);
-
-            if (eventListeners.TryGetValue(type, out var listeners))
-            {
-                for (int i = 0; i < listeners.Count; i++)
-                    listeners[i].Invoke(args);
-            }
+            foreach (var subscription in subscriptions)
+                subscription.Invoke(args);
         }
 
         public static void Subscribe(Action<TEvent> callback)
         {
-            var type = typeof(TEvent);
-
-            if (!eventListeners.ContainsKey(type))
-                eventListeners.Add(type, new List<Action<TEvent>>());
-
-            var listeners = eventListeners[type];
-
-            if (!listeners.Contains(callback))
-                listeners.Add(callback);
+            subscriptions.Add(callback);
         }
 
         public static void Unsubscribe(Action<TEvent> callback)
         {
-            var type = typeof(TEvent);
-
-            if (eventListeners.TryGetValue(type, out var listeners))
-                listeners.Remove(callback);
+            subscriptions.Remove(callback);
         }
     }
 }
